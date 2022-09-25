a58ab8b003df996be8cffe54d331b0d9dfd8524e
Zainan Victor Zhou <zzn+github@zzn.im>
2022-09-25 12:45:05 -0700

    Fix html

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,20 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>Zainan Victor Zhou</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+  <div class="zzn-main">
     <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      Lead author of ERC-1202, coder and community builder
     </p>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+.acf-main {
+  max-width: 600px;
+  margin: 0 auto;
+  text-align: justify;
+}
+.btn-sm {
+  padding: 2px 4px;
 }
 </style>
