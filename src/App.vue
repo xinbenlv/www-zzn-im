bc521ae36f1af81785b5f85e33d09dcb44adf5d0
Zainan Victor Zhou <zzn+github@zzn.im>
2022-09-26 15:43:56 -0700

    Fix some styling

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,6 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue';
-import { event, pageview } from 'vue-gtag'
-event('login', { method: 'Google' });
-pageview({ page_path: '/about' });
 </script>
 
 <template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,42 +9,33 @@
 
   </div>
   <div class="flex justify-center space-x-4 w-full">
-    <button class="bg-sky-500 hover:bg-sky-700"
+    <button class="bg-green-500 hover:bg-green-700 py-2 px-4 rounded-full"
             @click="contactBtn">
       Email
     </button>
-    <button class="bg-sky-500 hover:bg-sky-700"
+    <button class="bg-green-500 hover:bg-green-700 py-2 px-4 rounded-full"
             @click="bookTimeBtn">
       Book time
     </button>
   </div>
 
 </template>
-<script lang="ts">
-import { defineComponent } from 'vue'
-import {event} from 'vue-gtag';
+<script setup lang="ts">
+import { event, pageview } from 'vue-gtag';
+pageview({ page_path: '/' });
 
-export default defineComponent({
-  // type inference enabled
-  props: {
-  },
-  mounted() {
-  },
-  methods: {
-    contactBtn(event: Event) {
-      console.log(`Clicked contact button.`);
-      event('contact_btn_clicked');
-      window.open("mailto://contact@zzn.im", '_blank');
-    },
-    bookTimeBtn(event: Event) {
-      console.log(`Clicked bookTimeBtn button.`);
-      event('book_time_btn');
-      window.open("https://calendly.com/zainan/30min", '_blank');
-    }
-  },
+function contactBtn() {
+  console.log(`Clicked contact button.`);
+  event('contact_btn_clicked');
+  window.open("mailto://contact@zzn.im", '_blank');
+};
 
+function bookTimeBtn() {
+  console.log(`Clicked bookTimeBtn button.`);
+  event('book_time_btn');
+  window.open("https://calendly.com/zainan/15min", '_blank');
+};
 
-});
 </script>
 <style scoped>
 .zzn-main {
@@ -54,11 +45,5 @@ export default defineComponent({
 }
 .btn-sm {
   padding: 2px 4px;
-}
-.bg-sky-500 {
-  background-color: #0ea5e9;
-}
-.hover\:bg-sky-700:hover {
-  background-color: #0369a1;
 }
 </style>
